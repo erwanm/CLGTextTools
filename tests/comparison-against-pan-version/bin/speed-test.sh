6aa94d0962b145e7698ac9b811112ea4cb11fa28
Erwan Moreau <moreaue@scss.tcd.ie>
2015-10-17 14:28:12 +0100

    usage help speed test

--- tests/comparison-against-pan-version/bin/speed-test.sh
+++ tests/comparison-against-pan-version/bin/speed-test.sh
@@ -12,7 +12,7 @@ function generate {
     cat $tmpErr 1>&2
     rm -f $tmpErr
     echo "Time for $version:"
-    cat "$targetDir/time-$version.out"
+    cat "$targetDir/time-$version.out" | grep "."
 }
 
 
@@ -41,7 +41,7 @@ function load {
     cat $tmpErr 1>&2
     rm -f $tmpErr
     echo "Time for $version:"
-    cat "$targetDir/time-$version.out"
+    cat "$targetDir/time-$version.out" | grep "."
 }
 
 
@@ -49,7 +49,13 @@ function load {
 if [ $# -ne 3 ]; then
     echo "usage: <input dir> <output dir> <nb parallel>" 1>&2
     echo 1>&2
-    echo "  documents read from <input dir>/*/*txt" 1>&2
+    echo "  Runs a speed test which compares the PAN15 version with the 'new' version," 1>&2
+    echo "  i.e. the CLGTextTools version. The test consists in running:" 1>&2
+    echo "    - the generation of count files for a set of predefined obs types" 1>&2
+    echo "    - the loading of obs counts (both from count file or from reading" 1>&2
+    echo "      the doc file directly, for the 'new' version)" 1>&2
+    echo "  the second test (loading) is run for <nb parallel> subprocesses simultaneously." 1>&2
+    echo "  Input documents are read from <input dir>/*/*txt" 1>&2
     echo 1>&2
     exit 1
 fi
@@ -62,17 +68,19 @@ nbParallel="$3"
 for inputDoc in $inputDir/*/*txt; do
     sizeC=$(cat "$inputDoc" | wc -c)
     sizeW=$(cat "$inputDoc" | wc -w)
-    echo -n "Processing $inputDoc: $sizeC chars, $sizeW words."
+    echo "*** Processing $inputDoc: $sizeC chars, $sizeW words."
     countDir="$outputDir/count-files"
     [ -d "$countDir" ] || mkdir "$countDir" 
-    echo "Generating count files..."
+    echo "* Generating count files..."
     generate "$countDir" "$inputDoc" pan15
     generate "$countDir" "$inputDoc" new
     
     readDir="$outputDir/loading-process"
     [ -d "$readDir" ] || mkdir "$readDir" 
-    echo "Loading n-grams counts..."
+    echo " * Loading n-grams counts..."
     load "$readDir" "pan15" "read-pan15.sh '$inputDoc' '$countDir/pan15' '$readDir/pan15'" "$nbParallel"
     load "$readDir" "new-count-files" "read-new.sh '$inputDoc' 1 '$countDir/new' '$readDir/new-count-files'" "$nbParallel"
     load "$readDir" "new-input-doc" "read-new.sh '$inputDoc' 0 '$countDir/new' '$readDir/new-input-doc'" "$nbParallel"
+    echo
+    echo
 done
